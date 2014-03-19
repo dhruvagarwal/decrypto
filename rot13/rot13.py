6943348d739b21009dd40a3387ec98444f219816
dhruvagarwal <dhruvagga@gmail.com>
2014-03-20 01:58:31 +0530

    Updated rot13.py

--- rot13/rot13.py
+++ rot13/rot13.py
@@ -1,8 +1,5 @@
 import xerox
-import string
-rot13 = string.maketrans( 
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-s=raw_input('Enter text for rot13 - ')
-print string.translate(s, rot13)
-xerox.copy(string.translate(s, rot13))
+ans=raw_input('Enter text for rot13 - ').encode('rot13')
+print ans
+xerox.copy(ans)
+print 'Copied to clipboard'
