ecfe71808215bafdd76686f88844585af11d4939
Anmol Singh Jaggi <anmolj.65@gmail.com>
2015-10-02 05:27:16 +0530

    Modified ROT13.  - Added readme  - Added error-handling  - Added support for input through command-line arguments

---
+++ rot13/README.md
@@ -0,0 +1,14 @@
+**Description**  
+A Python script to demonstrate [ROT13][1] cipher.
+
+**Usage**  
+`python rot13.py "Hello world!!"`
+
+**Requirements**
+ - Python2
+ 
+**Dependencies**
+ - xerox - `pip install xerox`
+ - xclip - `apt-get install xclip`
+
+[1]:https://en.wikipedia.org/wiki/ROT13
--- rot13/rot13.py
+++ rot13/rot13.py
@@ -1,5 +1,22 @@
+import sys
 import xerox
-ans=raw_input('Enter text for rot13 - ').encode('rot13')
-print ans
-xerox.copy(ans)
-print 'Copied to clipboard'
+
+def main():
+	try:
+		plainText = ""
+		if (len(sys.argv) == 1):
+			plainText = raw_input('Enter text -:\n')
+		else:
+			plainText = sys.argv[2]
+	
+		cipherText = plainText.encode('rot13')
+		print cipherText
+	
+		xerox.copy(cipherText)
+		print 'Copied to clipboard!!'
+	except Exception as exception:
+		errorMessage = "Error : " + exception.message
+		print errorMessage
+
+if __name__ == '__main__':
+	main()
